c3ff54e0f4889e49f5ab6f175b9b2ab2a8ce477f
Gabriel Quispe Rosas <gabrielomarquisperosas@gmail.com>
2019-12-01 14:12:28 -0500

    fixing dynamic route

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -19,6 +19,7 @@ export class FormComponent implements OnInit {
   constructor(private platoService: PlatoService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.cargarPlato()
   }
 
   cargarPlato(): void {
